41bb728908fdd63e53aedc5aeb488f3cb684b307
Katy Yut <98367085+kyut0@users.noreply.github.com>
2022-12-13 17:41:43 +0000

    Challenge 2

--- notes.sql
+++ notes.sql
@@ -6,3 +6,9 @@
 -- restaurant.db SQLite database.
 SELECT * FROM Dishes;
 SELECT * FROM Customers;
+
+-- To create a table, use the following structure:
+CREATE TABLE TableName (
+  Field1 DATATYPE,
+  Field2 DATATYPE
+);
