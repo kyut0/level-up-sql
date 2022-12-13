be02915d47614ddf8cf590565dac1ee2be969827
Katy Yut <98367085+kyut0@users.noreply.github.com>
2022-12-13 20:32:35 +0000

    Challenge 6

--- 02_library/challenges/02_06_visitreminder_ch.sql
+++ 02_library/challenges/02_06_visitreminder_ch.sql
@@ -1,2 +1,8 @@
 -- Prepare a report of the library patrons
 -- who have checked out the fewest books.
+
+SELECT Patrons.FirstName, Patrons.Email, COUNT(Loans.BookID) AS NumBooks
+FROM Patrons
+JOIN Loans ON Patrons.PatronID = Loans.PatronID
+GROUP BY Patrons.PatronID
+ORDER BY NumBooks;
