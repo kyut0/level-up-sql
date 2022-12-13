89fada1b6da957ec05fd11b5b239813edcb8b3b2
Katy Yut <98367085+kyut0@users.noreply.github.com>
2022-12-13 20:47:43 +0000

    Challenge 8

--- 02_library/challenges/02_08_bookstats_ch.sql
+++ 02_library/challenges/02_08_bookstats_ch.sql
@@ -3,5 +3,16 @@
 -- Report 1: Show how many titles were published 
 -- in each year.
 
+SELECT COUNT(DISTINCT(Title)) AS PubCount, Published
+FROM Books
+GROUP BY Published
+ORDER BY PubCount DESC;
+
 -- Report 2: Show the five books that have been
 -- checked out the most.
+SELECT Books.Title, COUNT(Loans.LoanID) AS LoanCount
+FROM Loans
+JOIN Books ON Books.BookID = Loans.BookID
+GROUP BY Books.Title
+ORDER BY LoanCount DESC
+LIMIT 5;
