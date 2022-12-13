b5edc48f509b978d9471a1accbcb80e545a22dfe
Katy Yut <98367085+kyut0@users.noreply.github.com>
2022-12-13 18:37:26 +0000

    Challenge 8

--- 01_restaurant/challenges/01_08_findreservation_ch.sql
+++ 01_restaurant/challenges/01_08_findreservation_ch.sql
@@ -5,3 +5,11 @@
 -- Stevensen, Stephensen, Stevenson, Stephenson, Stuyvesant
 
 -- There are four people in the party. Today is June 14th.
+
+-- Join the Customers table to the Reservations table
+SELECT Customers.FirstName, Customers.LastName, Reservations.Date, Reservations.PartySize
+FROM Reservations
+JOIN Customers ON Customers.CustomerID = Reservations.CustomerID
+WHERE Customers.LastName LIKE "St%" 
+AND PartySize = 4
+ORDER BY Reservations.Date DESC;
