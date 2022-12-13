66b476221015ea508026700b148d0b7429300600
Katy Yut <98367085+kyut0@users.noreply.github.com>
2022-12-13 19:58:16 +0000

    Challenge 1

--- 02_library/challenges/02_01_checkavailability_ch.sql
+++ 02_library/challenges/02_01_checkavailability_ch.sql
@@ -1,2 +1,19 @@
 -- Determine how many copies of the book 'Dracula'
 -- are available for library patrons to borrow.
+
+SELECT COUNT(DISTINCT Books.Barcode) 
+FROM Loans
+JOIN Books ON Loans.BookID = Books.BookID
+WHERE Books.Title == "Dracula" 
+  AND Loans.ReturnedDate != NULL;
+
+SELECT 
+  (SELECT COUNT(Books.Title)
+    FROM Books
+    WHERE Books.Title = "Dracula") - 
+  (SELECT COUNT(Books.Title)
+    FROM Loans
+    JOIN Books ON Loans.BookID = Books.BookID
+    WHERE Books.Title == "Dracula"
+    AND Loans.ReturnedDate IS NULL)
+  AS AvailableCopies;
