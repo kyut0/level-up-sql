50cf38df34457d0ff5f13b5932a2ad40332fba57
Katy Yut <98367085+kyut0@users.noreply.github.com>
2022-12-13 18:46:51 +0000

    Challenge 9

--- 01_restaurant/challenges/01_09_createreservation_ch.sql
+++ 01_restaurant/challenges/01_09_createreservation_ch.sql
@@ -4,3 +4,27 @@
 -- Use the following information:
 -- Sam McAdams (smac@kinetecoinc.com), for 5 people
 -- on August 12, 2022 at 6PM (18:00)
+
+SELECT *
+FROM Customers
+WHERE FirstName = "Sam" AND LastName = "McAdams";
+-- Returns nothing, so Sam needs to be added to the Customers table
+
+-- Add Sam to Customers table
+INSERT INTO Customers (FirstName, LastName, Email)
+VALUES ("Sam", "McAdams", "smac@kinetecoinc.com");
+
+-- Add Sam's reservation to the Reservations table
+INSERT INTO Reservations (CustomerID, Date, PartySize)
+VALUES (
+  (SELECT CustomerID
+    FROM Customers
+    WHERE FirstName = "Sam" AND LastName = "McAdams"),
+  "2022-08-12 18:00:00",
+  5
+);
+
+-- Check to see if Reservations table was updated
+SELECT *
+FROM Reservations
+ORDER BY ReservationID DESC;
