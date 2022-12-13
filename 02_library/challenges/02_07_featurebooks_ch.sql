c39bc9c218ef3c50f32dae791dbfe215c3a416af
Katy Yut <98367085+kyut0@users.noreply.github.com>
2022-12-13 20:38:56 +0000

    Challenge 7

--- 02_library/challenges/02_07_featurebooks_ch.sql
+++ 02_library/challenges/02_07_featurebooks_ch.sql
@@ -2,3 +2,12 @@
 
 -- Make a pick list of books published from 1890-1899 
 -- which are not currently checked out.
+
+SELECT Books.Title
+FROM Books
+JOIN Loans ON Books.BookID = Loans.LoanID
+WHERE Books.Published >= "1890" AND Books.Published <= "1899"
+  AND Loans.ReturnedDate IS NOT NULL
+ORDER BY Books.Title;
+
+-- Apparently this approach has some issues?
