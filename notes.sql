eb2a79db97c7b11e6a402699765e10e0433cb496
Katy Yut <98367085+kyut0@users.noreply.github.com>
2022-12-13 18:02:30 +0000

    Challenge 4

--- notes.sql
+++ notes.sql
@@ -12,3 +12,7 @@ CREATE TABLE TableName (
   Field1 DATATYPE,
   Field2 DATATYPE
 );
+
+-- To insert data into existing table:
+INSERT INTO TableName (Field1, Field2, ..., FieldN)
+VALUES ("Value1", "Value2", ..., "ValueN");
