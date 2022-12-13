14f0a7f86a65fe3664f50d422f0de91d5b51e060
Katy Yut <98367085+kyut0@users.noreply.github.com>
2022-12-13 20:25:45 +0000

    Challenge 5

--- 02_library/challenges/02_05_returnbooks_ch.sql
+++ 02_library/challenges/02_05_returnbooks_ch.sql
@@ -4,3 +4,16 @@
 -- Book 1: 6435968624
 -- Book 2: 5677520613
 -- Book 3: 8730298424
+
+UPDATE Loans
+SET ReturnedDate = "2022-07-05"
+WHERE ReturnedDate IS NULL
+  AND BookID IN 
+    (SELECT BookID
+      FROM Books
+      WHERE Barcode IN (6435968624, 5677520613, 8730298424)
+    );
+
+SELECT *
+FROM Loans
+ORDER BY ReturnedDate DESC
